fd4ab3425954a77143e7a17d83edea4f11428f76
arunmadisetty <arun826@live.com>
2024-07-27 10:01:13 -0500

    provider file

--- keypair.tf
+++ keypair.tf
@@ -2,3 +2,8 @@ resource "aws_key_pair" "arun" {
   key_name   = var.key_name
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN79oshIfreAQxprkI/5Yddbqq92BXaLS9vYpeR9ggpa arunmadisetty@Aruns-MacBook-Pro.local"
 }
+
+variable "key_name" {
+    type = string
+    default = "terraform-github"
+}
